2dbe55da3ffd1bbe8bebf917a520a08028211442
“PedroJimenez73” <pjimenez@qualitycw.com>
2021-09-14 21:44:29 +0200

    links

--- src/slides/Slide102.js
+++ src/slides/Slide102.js
@@ -33,7 +33,21 @@ class Slide extends Component {
                         />
                 <div className="flex">
                     <div className="col-100">
-
+                        <p><b>The Lean Startup</b></p>
+                        <p>Página web de Eric Ries con información sobre los libros, artículos y herramientas de uso de <i>Lean Startup</i>.</p>
+                        <p>
+                            <a href="http://theleanstartup.com/" rel="noreferrer" target="_blank">http://theleanstartup.com/</a>
+                        </p>
+                        <p><b>¿Por qué <i>lean startup</i> lo va a cambiar todo? Néstor Guerra en TEDxSevilla</b></p>
+                        <p>¿Puede la metodología <i>LEAN</i> cambiar el mundo? Este emprendedor plantea que sí en este video de la iniciativa TED.  </p>
+                        <p>
+                            <a href="https://www.youtube.com/watch?v=E62ecUVZa9Q" rel="noreferrer" target="_blank">https://www.youtube.com/watch?v=E62ecUVZa9Q</a>
+                        </p>
+                        <p><b>Why Lean Startup changes everything</b></p>
+                        <p>Artículo sobre el potencial del <i>Lean Startup</i>. </p>
+                        <p>
+                            <a href="https://hbr.org/2013/05/why-the-lean-start-up-changes-everything" rel="noreferrer" target="_blank">https://hbr.org/2013/05/why-the-lean-start-up-changes-everything</a>
+                        </p>
                     </div>
                 </div>
             </div>
--- src/slides/Slide103.js
+++ src/slides/Slide103.js
@@ -33,7 +33,15 @@ class Slide extends Component {
                         />
                 <div className="flex">
                     <div className="col-100">
-
+                        <ul>
+                            <li>Azurmendi, A. (2011). <i>Derecho de la Comunicación</i>. Bosch. </li>
+                            <li><i>Constitución española de 1978</i></li>
+                            <li><i>Declaración de los Derechos Humanos</i> – Organización de las Naciones Unidas</li>
+                            <li>Escobar de la Serna, L. (2004). <i>Derecho de la Información</i>. Dykinson. </li>
+                            <li><i>Ley Orgánica 10/1995, de 23 de noviembre</i>, del Código Penal</li>
+                            <li>Miserachs Sala, P. (2014). <i>Estudios sobre la propiedad intelectual y la sociedad de la información: entre la ley y la utopía</i>. Atelier libros jurídicos.</li>
+                            <li>Sanjurjo Rebollo, B. (2009). <i>Manual de Derecho de la Información</i>. Dykinson. </li>
+                        </ul>
                     </div>
                 </div>
             </div>
